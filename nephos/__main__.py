a70b67ec48920b45f9574c783f3012f3509d471b
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-05-17 00:12:40 +0530

    Switch from pip to pipenv

--- nephos/__main__.py
+++ nephos/__main__.py
@@ -5,13 +5,9 @@ The file run when Nephos is called as a module
 from . import first_time
 import logging
 from .load_config import Config
-from dotenv import load_dotenv
 
 # Here logger defined manually since this is the first file launched and has __name__ = "__main__"
 log = logging.getLogger('nephos')
-
-# loading environment variables from .env file
-load_dotenv(dotenv_path='nephos/.env', verbose=True)
 
 
 def main():  # TODO: More work on this, temporary
