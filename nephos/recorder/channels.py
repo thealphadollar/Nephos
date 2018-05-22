0027ff7b06e26a4befb8e618816fa0b7089aecc4
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-05-22 23:20:20 +0530

    Complete channel operations code

--- nephos/recorder/channels.py
+++ nephos/recorder/channels.py
@@ -4,8 +4,11 @@ Manages all operations related to channels, including adding, deleting and updat
 from logging import getLogger
 from sqlite3 import Error
 import click
+import subprocess
+import os
 from ..manage_db import DBHandler
 from ..custom_exceptions import DBException
+from .. import __recording_dir__
 
 LOG = getLogger(__name__)
 
@@ -18,10 +21,10 @@ class ChannelHandler:
     @click.command()
     @click.option("--name", prompt="Channel name", help="name of the channel")
     @click.option("--ip", prompt="IP", help="ip address of the channel")
-    @click.option("--country", prompt="Country codes [separated by space]", help=
-                  "country codes for the channel")
-    @click.option("--lang", prompt="Language codes [separated by space]", help=
-                  "available languages in the channel")
+    @click.option("--country", prompt="Country codes [separated by space]",
+                  help="country codes for the channel")
+    @click.option("--lang", prompt="Language codes [separated by space]",
+                  help="available languages in the channel")
     @click.option("--tmz", prompt="Timezone", help="timezone of the channel")
     def add_channel(self, name, ip, country, lang, tmz):
         """
@@ -83,6 +86,9 @@ class ChannelHandler:
         for channel in ch_data:
             ch_id = DBHandler.insert_data(db_cur, "channels", channel)
             LOG.info("New channel (id = %s) added with following data:\n%s", ch_id, channel)
+            # create directory for channel recordings
+            ch_dir = os.path.join(__recording_dir__, channel["name"])
+            os.makedirs(ch_dir, exist_ok=True)
 
     @staticmethod
     def delete_channels(ch_ip_name):
@@ -128,7 +134,7 @@ class ChannelHandler:
             LOG.error(err)
 
     @staticmethod
-    def record(ip_addr, addr, duration):
+    def record(ip_addr, addr, duration_secs):
         """
         Function to record stream from the ip address for the given duration,
         and in the given addr.
@@ -141,10 +147,42 @@ class ChannelHandler:
         addr
             type: str
             absolute file path to save the recording
-        duration
+        duration_secs
+            type: int
+            duration to record the show in seconds
 
         Returns
         -------
 
         """
-        pass
+        if not _is_up(ip_addr):
+            return
+
+        duration_27khz = int(duration_secs * 27000000)
+        timeout_str = '-d {:d}'.format(duration_27khz)
+        cmd = "multicat {duration} -u @{channel_ip} {out_file}".format(
+            duration=timeout_str, channel_ip=ip_addr, out_file=addr)
+        try:
+            subprocess.check_call(cmd, shell=False)
+        except subprocess.CalledProcessError as err:
+            LOG.warning("Recording for channel with ip %s", ip_addr)
+            LOG.error(err)
+
+
+def _is_up(ip):
+    """
+    Queries if the channel was up in the previous test.
+
+    Returns
+    -------
+        type: bool
+        True, if the channel was up, False otherwise.
+
+    """
+    command = """SELECT status FROM channels WHERE ip=?"""
+    with DBHandler.connect() as db_cur:
+        status = db_cur.execute(command, ip)
+
+    if status == "up":
+        return True
+    return False
--- nephos/scheduler.py
+++ nephos/scheduler.py
@@ -7,14 +7,16 @@ from logging import getLogger
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor
-from pytz import utc
+import time
 from . import __nephos_dir__
+from .recorder.channels import ChannelHandler
 
 LOG = getLogger(__name__)
 
 # config for the scheduler, not to be set by the user
 PATH_JOB_DB = os.path.join(__nephos_dir__, "databases/jobs.db")
 MAX_CONCURRENT_JOBS = 20
+TMZ = time.tzname[time.daylight]
 
 
 class Scheduler:
@@ -32,7 +34,8 @@ class Scheduler:
             'default': ThreadPoolExecutor(MAX_CONCURRENT_JOBS)
         }
 
-        self._scheduler = BackgroundScheduler(jobstores=job_stores, executors=executors)
+        self._scheduler = BackgroundScheduler(jobstores=job_stores, executors=executors,
+                                              timezone=TMZ)
         LOG.info("Scheduler initialised with database at %s", PATH_JOB_DB)
 
     def start(self):
@@ -45,4 +48,67 @@ class Scheduler:
         self._scheduler.start()
         LOG.info("Scheduler started!")
 
-    
+    def add_recording_job(self, ip, out_path, duration, job_time, week_days, job_name):
+        """
+        Add recording jobs to the scheduler
+
+        Parameters
+        ----------
+        ip
+            type: str
+            ip address of the channel
+        out_path
+            type: str
+            path where the file is to the saved
+        duration
+            type: int
+            duration in minutes for the job to run
+        job_time
+            type: HH:MM
+            time to begin the job
+        week_days
+            type: str
+            contains the details for which week days the job is to run
+        job_name
+            type: str
+            name of the job, unique
+
+        Returns
+        -------
+
+        """
+        hour, minute = job_time.split(":")
+        duration_secs = 60 * duration
+        job = self._scheduler.add_job(ChannelHandler.record(ip_addr=ip, addr=out_path,
+                                                            duration=duration_secs), trigger='cron',
+                                      hour=hour, minute=minute, day_of_week=week_days, id=job_name,
+                                      max_instances=1)
+        LOG.info("Recording job added: %s", job)
+
+    def add_maintenance_jobs(self, func, main_id, interval):
+        """
+        Add maintenance jobs to the scheduler
+
+        Parameters
+        ----------
+        func
+            type: callable
+            maintenance function to be run at the execution of the job
+        main_id
+            type: str
+            unique id to be associated with the job
+        interval
+            type: int
+            repetition interval in minutes
+
+        Returns
+        -------
+
+        """
+        job = self._scheduler.add_job(func=func, trigger='interval',
+                                      minutes=interval, id=main_id, max_instances=1, )
+        LOG.info("Maintenance job added %s", job)
+
+
+
+
