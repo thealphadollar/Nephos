dd4ab5c91be146e445638c454f9f3ce197d9ba43
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-07-13 11:37:21 +0530

    fixes #48, stores recordings in path address channame_%Y-%m-%d-%H-%M

--- nephos/preprocessor/preprocess.py
+++ nephos/preprocessor/preprocess.py
@@ -92,7 +92,7 @@ class PreprocessHandler:
         try:
             with DBHandler.connect() as db_cur:
                 ch_name = PreprocessHandler._get_channel_name(ip_addr, db_cur)
-                store_path = os.path.join(__upload_dir__, ch_name + "-" + str(datetime.now().strftime("%Y-%m-%d")))
+                store_path = os.path.join(__upload_dir__, ch_name + "_" + str(datetime.now().strftime("%Y-%m-%d-%H-%M")))
                 lang, sub_lang = ApplyProcessMethods.get_lang(orig_path)
 
                 data = {
