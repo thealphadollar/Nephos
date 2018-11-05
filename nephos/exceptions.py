a894be4f34ecc9d9a8a4df067ccc85a32c785149
T1duS <udit121sanghi@gmail.com>
2018-11-06 00:15:03 +0530

    Improved Docs

--- nephos/exceptions.py
+++ nephos/exceptions.py
@@ -42,7 +42,7 @@ class SingleInstanceException(BaseException):
 
 class ProcessFailedException(Exception):
     """
-    Handles exceptions concerned with a failure of preprocessing
+    Handles exceptions concerned with failures of preprocessing
     """
     def __init__(self, path_to_file, store_path, db_cur, error):
         """
