f19576ca6de4788fe0641d2cdc40e534f135c437
T1duS <udit121sanghi@gmail.com>
2018-12-05 21:25:45 +0530

    Removed extra file

--- nephos_start.sh
+++ nephos_start.sh
@@ -5,5 +5,4 @@ NEPHOS_DIR=$(dirname $0)
 cd ${NEPHOS_DIR}
 export PYTHON_BIN_PATH="$(python3 -m site --user-base)/bin"
 export PATH="$PATH:$PYTHON_BIN_PATH"
-sudo apt install screen
 screen pipenv run python3 -m nephos start
